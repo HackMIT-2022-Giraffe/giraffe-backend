0ecca176a3241a0e47fccac380a60c56f8eb2377
Rohan Mehta <rmehta98@gatech.edu>
2022-10-02 09:18:16 -0400

    add bullet summary

--- index.js
+++ index.js
@@ -2,18 +2,30 @@ const express = require('express')
 const { OpenAIApi } = require('openai')
 const {  bulletSummary } = require('./bulletSummary')
 const { OpenAIConfiguration } = require('./config')
-const bodyParser = require("body-parser");
+const cors = require('cors');
 
 const app = express()
-const port = 3000
-app.use(bodyParser.urlencoded({ extended: false}))
-app.use(bodyParser.json())
+const port = 3001
+
+app.use(cors({
+  origin: "*"
+}))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Setting up of OpenAI
 const openai = new OpenAIApi(OpenAIConfiguration);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.post('/bullet', async (req, res) => {
+  console.log("begin")
+  let simpText = req.body.simpText;
+  let bulletResp = await bulletSummary(openai, simpText);
+  console.log(typeof bulletResp)
+  console.log("end")
+  res.send({
+    "bullets": bulletResp.data.choices[0].text.split(". ")
+  })
 })
 
 app.listen(port, () => {
